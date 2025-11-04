df3b656baae22183c66167a0df272586879991c8
nancyep-ship-it <nancyep@gmail.com>
2025-11-03 20:27:45 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function App() {
+  return (
+    <div style={{fontFamily:'sans-serif', padding:'2rem', background:'#f3ece9'}}>
+      <h1>Hello Trackers</h1>
+      <p>This is your React wellness tracker app structure ready for GitHub & Vercel.</p>
+    </div>
+  );
+}
